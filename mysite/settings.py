f356437c2eff8c042ba7192ae0ad346b2963117e
leeyongjoo <2yongjooo@gmail.com>
2021-01-06 16:24:32 +0900

    fix: STATIC_ROOT, STATICFILES_DIRS 추가

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,4 +121,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'static'
+)
